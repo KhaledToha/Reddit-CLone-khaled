ffe7305f1299d33fe77f8058f25f2f9b5a9052f0
KhaledToha <khaled.f.toha@gmail>
2023-04-29 20:49:46 +0200

    signup validation improving

--- server/validation/authValidation.js
+++ server/validation/authValidation.js
@@ -4,13 +4,13 @@ const Joi = require('joi');
 exports.SignUpSchema = Joi.object({
 
 
-    name: Joi.string().alphanum().required().min(3).max(50),
+    name: Joi.string().required().min(3).max(50),
     email: Joi.string()
         .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
     password: Joi.string().pattern(new RegExp('[a-zA-Z0-9]{6,30}$')),
     confirmPassword: Joi.ref('password'),
     type: Joi.string().valid('admin','user'),
-    phone: Joi.string().pattern(new RegExp('059{1}[95287]{1}[0-9]{6}')),
+    phone: Joi.string().pattern(new RegExp('05[9,6]{1}[0-9]{1}[0-9]{6}')),
     img_url: Joi.string()
 })
 
